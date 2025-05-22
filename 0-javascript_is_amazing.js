5dc6823d017bc4563f440352fe84fb2c71af1160
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-05-22 11:07:14 +0000

    Add script to print Javascript is amazing

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,2 +1,12 @@
+
+// Declare a constant variable
 const myVar = "JavaScript is amazing";
-console.log(myVar);
+
+// Initialize a counter
+let count = 0;
+
+// Use a while loop to print the message 3 times
+while (count < 3) {
+    console.log(myVar);
+    count++;
+}
