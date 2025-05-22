085332c67f6bea2f1c0a21dd1466f0806c719981
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-05-22 12:20:38 +0000

    Add 0-_javascript_is_amazing.js

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,11 @@
+// Declare a constant variable
+const myVar = "JavaScript is amazing";
+
+// Initialize a counter
+let count = 0;
+
+// Use a while loop to print the message 3 times
+while (count < 3) {
+    console.log(myVar);
+    count++;
+}
