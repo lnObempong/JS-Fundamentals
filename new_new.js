a9cce6837b56aec08dccf6c85c37552e8dbd70f7
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-05-22 13:08:57 +0000

    Delete 0-javascrip_is_amazing.js for a fix repeated output

--- new_new.js
+++ new_new.js
@@ -1,11 +1,2 @@
-// Declare a constant variable
-const myVar = "JavaScript is amazing";
-
-// Initialize a counter
-let count = 0;
-
-// Use a while loop to print the message 3 times
-while (count < 3) {
-    console.log(myVar);
-    count++;
-}
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
